af90106ee4be29c543c42efb6533d380dfd2c93d
vhail <anthonyvanluc@gmail.com>
2017-09-11 23:29:17 -0400

    update execute.cpp

--- execute.cpp
+++ execute.cpp
@@ -5,17 +5,25 @@
 //execute after detection of created, deleted, or modified event
 void	   execute(const char *action) {
 	char *basepath, *fullpath, *event, *timestamp;
+	char command[BUFSIZ];
 
 	basepath = getenv("BASEPATH");
 	fullpath = getenv("FULLPATH");
 	event = getenv("EVENT");
 	timestamp = getenv("TIMESTAMP");
 
+	cout << "base: " << basepath << " full: " << fullpath << " event: " << event << " time :" << timestamp << endl;
+
+	if (!sprintf(command, "%s %s", basepath, action)) {
+		cout << "fail sprintf" << endl;
+	}
+	cout << "command:" << command << endl;
+
 	cout << "Executing action " << action << " on " << basepath << endl;
 
-	// if (execvp(basepath, action)) {
-	// 	cout << "Success" << endl;
-	// } else {
-	// 	cout << "execvp failed: " << strerror(errno) << endl;
-	// }
+	if (execvp(basepath, (char* const*)command)) {
+		cout << "Success" << endl;
+	} else {
+		cout << "execvp failed: " << strerror(errno) << endl;
+	}
 }
