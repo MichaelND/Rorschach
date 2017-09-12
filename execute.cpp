f213edb03b9da2c8c1124c21e8edb4dc30331068
donaldvluc <donaldvluc@gmail.com>
2017-09-12 00:24:07 -0400

    Replaced  and

--- execute.cpp
+++ execute.cpp
@@ -14,15 +14,32 @@ void	   execute(const char *action) {
 
 	cout << "base: " << basepath << " full: " << fullpath << " event: " << event << " time :" << timestamp << endl;
 
-	if (!sprintf(command, "%s %s", basepath, action)) {
-		cout << "fail sprintf" << endl;
+	// Replace BASEPATH and FULLPATH.
+	string cppaction(action);
+	size_t b_pos = cppaction.find("${BASEPATH}");
+	cppaction.replace(b_pos, string("${BASEPATH}").length(), basepath);
+	size_t f_pos = cppaction.find("${FULLPATH}");
+	cppaction.replace(f_pos, string("${FULLPATH}").length(), fullpath);
+
+	int i = 0;
+	char 
+	for (char *tok = strtok(cppaction.c_str(), " "); tok != NULL; tok = strtok(NULL, " ")) {
+
 	}
-	cout << "command:" << command << endl;
 
-	cout << "Executing action " << action << " on " << basepath << endl;
+	char garbo[BUFSIZ];
+	sscanf(action, "%s %[^\n]", command, garbo);
 
-	if (execvp(basepath, (char* const*)command)) {
-		cout << "Success" << endl;
+	// if (!sprintf(command, "%s %s", basepath, cppaction)) {
+	// 	cout << "fail sprintf" << endl;
+	// }
+	cout << "command: " << command << endl;
+
+	cout << "Executing action " << cppaction << " on " << basepath << endl;
+
+
+	if (execvp(command, (char* const*)(cppaction.c_str()))) {
+		cout << "Success: " << cppaction << endl;
 	} else {
 		cout << "execvp failed: " << strerror(errno) << endl;
 	}
